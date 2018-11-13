92f578fe770e484629cf4479e0a11b752b098488
TatsumiKei <ki3ttm@gmail.com>
2018-11-13 11:43:16 +0900

    大きさに応じて質量が変わるようにし、時間で小さくなっていく機能を導入

---
+++ Assets/Scripts/Tatsumi/TimeEventValue.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeEventValue : MonoBehaviour {
+	[SerializeField, Tooltip("対象")]
+	EventValue target = null;
+	[SerializeField, Tooltip("変化量")]
+	float timeVal = 0.0f;
+
+	void FixedUpdate() {
+		target.Val += (timeVal * Time.fixedDeltaTime);
+	}
+}
