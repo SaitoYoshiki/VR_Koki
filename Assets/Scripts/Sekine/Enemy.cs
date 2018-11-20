8c3818df48708a4b35ca3fa3a1646e1b880e26e6
sekine <sekine.soushihal0829@gmail.com>
2018-11-20 10:24:51 +0900

    敵のプレハブ作成、Inspectorで設定したオブジェクトに追従

---
+++ Assets/Scripts/Sekine/Enemy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+	public GameObject playerObj;
+
+	private Rigidbody rb;
+
+	private float MoveSpeed = 10.0f;
+
+	void Start ()
+	{
+		rb = this.GetComponent<Rigidbody>();
+	}
+	
+	void Update ()
+	{
+		
+	}
+
+	void FixedUpdate()
+	{
+		Move();
+	}
+
+	public void Delete()
+	{
+		Destroy(this.gameObject);
+	}
+
+	void Move()
+	{
+		//プレイヤーの方向を向く
+		Vector3 vec = playerObj.transform.position - this.transform.position;
+		this.transform.rotation = Quaternion.LookRotation(vec);
+
+		//回転させる
+		rb.AddTorque(this.transform.right * MoveSpeed);
+	}
+
+}
