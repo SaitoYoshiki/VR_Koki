42aa29e15fdf2599965a62bd7b77ec2d56c9aec7
K <ki3ttm@gmail.com>
2018-12-06 11:32:10 +0900

    弾かれる機能を追加

--- Assets/Scripts/Tatsumi/CollisionBound.cs
+++ Assets/Scripts/Tatsumi/CollisionBound.cs
@@ -5,20 +5,43 @@ using UnityEngine;
 public class CollisionBound : MonoBehaviour {
 	[SerializeField]
 	Rigidbody rb = null;
-	[SerializeField, Tooltip("倍率")]
-	float rate = 1.0f;
+	[SerializeField, Tooltip("被吹っ飛び倍率")]
+	float boundRate = 1.0f;
+	[SerializeField, Tooltip("打ち勝ち度")]
+	float powerfulRate = 1.0f;
+
+	[SerializeField]
+	Enemy enemy = null;
+	[SerializeField]
+	float lastBoundTime = 0.0f;
+	[SerializeField]
+	float boundEnemyStopTime = 1.0f;
+
+	private void FixedUpdate() {
+		if (enemy && !enemy.enabled) {
+			if (Time.time > (lastBoundTime + boundEnemyStopTime)) {
+				enemy.enabled = true;
+			}
+		}
+	}
 
 	private void OnCollisionEnter(Collision _col) {
 		Rigidbody colRb = _col.gameObject.GetComponent<Rigidbody>();
 		if (!colRb) return;
+		CollisionBound colBound = _col.gameObject.GetComponent<CollisionBound>();
+		if (!colBound) return;
 
 		Vector3 selfToColVec = (_col.transform.position - transform.position).normalized;
 
 		float selfPower = Vector3.Dot(rb.velocity, selfToColVec);
 		float colPower = Vector3.Dot(colRb.velocity, -selfToColVec);
-		if ((selfPower * rb.mass) < (colPower * colRb.mass)) {
-			Debug.Log("Hit Lose " + name);
-			rb.AddForce((-selfToColVec * colPower * rate), ForceMode.VelocityChange);
+		if ((selfPower * rb.mass * powerfulRate) < (colPower * colRb.mass * colBound.powerfulRate)) {
+			Debug.Log("Hit Lose " + name + "\n" + (-selfToColVec * colPower * boundRate));
+			rb.AddForce((-selfToColVec * colPower * boundRate), ForceMode.VelocityChange);
+			lastBoundTime = Time.time;
+			if (enemy) {
+				enemy.enabled = false;
+			}
 		}
 		else {
 			Debug.Log("Hit Win " + name);
---
+++ Assets/Scripts/Tatsumi/LimiVelocity.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimiVelocity : MonoBehaviour {
+	[SerializeField]
+	Rigidbody rb = null;
+	[SerializeField]
+	float limit = 1.0f;
+	[SerializeField, ReadOnly]
+	float prevVel = 0.0f;
+
+	void Update() {
+		if (rb.velocity.magnitude > limit) {
+			rb.velocity = (rb.velocity.normalized * limit);
+		}
+		prevVel = rb.velocity.magnitude;
+	}
+}
