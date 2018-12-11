1a77dee8d53cf12b8dea84280fd27c0fefc1795a
SaitoYoshiki <saitosaito888888@gmail.com>
2018-12-12 03:55:58 +0900

    とりあえずのゲームループを作成

---
+++ Assets/Scripts/Saito/GameLoop.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLoop : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(GameLoopCoroutine());
+    }
+    
+    private void Update()
+    {
+    }
+
+    public IEnumerator GameLoopCoroutine()
+    {
+        yield return StartCoroutine(GameStartCoroutine());
+        
+        Debug.Log("ゲーム：開始");
+        yield return new WaitForSeconds(2.0f);
+        Debug.Log("ゲーム：終了");
+
+        yield return StartCoroutine(GameEndCoroutine());
+    }
+
+
+    public IEnumerator GameStartCoroutine()
+    {
+        Debug.Log("ゲーム開始の演出：開始");
+        yield return new WaitForSeconds(2.0f);
+        Debug.Log("ゲーム開始の演出：終了");
+    }
+
+    public IEnumerator GameEndCoroutine()
+    {
+        Debug.Log("ゲーム終了の演出：開始");
+        yield return new WaitForSeconds(2.0f);
+        Debug.Log("ゲーム終了の演出：終了");
+    }
+}
