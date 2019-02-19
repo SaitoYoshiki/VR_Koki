f8b776953f04dd6b5352da30037f05f972a7ee4b
YamadaNatsuki <72ch0opper@gmail.com>
2019-02-19 12:41:46 +0900

    円形フェード作成（カメラリグのeyeの子供に設定） 敵位置情報キャンバスをプリファブ化 全ステージに敵位置情報キャンバスを配置

---
+++ Assets/Scripts/Yamada/CircleFade.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleFade : MonoBehaviour{
+    [SerializeField, Tooltip("フェードの速さ"), Range(0.001f, 0.05f)]
+    private float speed;
+
+    public enum FadeState
+    {
+        SetIn,
+        SetOut,
+        In,
+        Out,
+        Complete,
+        None
+    }
+
+    private SpriteMask fadeMask;
+    private FadeState state = FadeState.None;
+
+    // Use this for initialization
+    void Start () {
+        fadeMask = GetComponent<SpriteMask>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        switch (state)
+        {
+            case FadeState.SetIn:
+                fadeMask.alphaCutoff = 0;
+                state = FadeState.In;
+                break;
+
+            case FadeState.In:
+                if(fadeMask.alphaCutoff < 1)
+                {
+                    fadeMask.alphaCutoff += speed;
+                }
+                else
+                {
+                    state = FadeState.Complete;
+                }
+                break;
+
+            case FadeState.SetOut:
+                fadeMask.alphaCutoff = 1;
+                state = FadeState.Out;
+                break;
+
+            case FadeState.Out:
+                if (fadeMask.alphaCutoff > 0)
+                {
+                    fadeMask.alphaCutoff -= speed;
+                }
+                else
+                {
+                    state = FadeState.Complete;
+                }
+                break;
+
+            case FadeState.Complete:
+                break;
+        }
+
+        if (Input.GetKeyDown(KeyCode.F1))
+            ChangeFadeState = FadeState.SetIn;
+
+        if (Input.GetKeyDown(KeyCode.F2))
+            ChangeFadeState = FadeState.SetOut;
+	}
+
+    public FadeState ChangeFadeState
+    {
+        get { return state; }
+        set { if (state == FadeState.None || state == FadeState.Complete) state = value; }
+    }
+}
