90887a9109ce2d0f27f3ffcfb647b8992163e75f
TatsumiKei <ki3ttm@gmail.com>
2019-02-19 17:01:23 +0900

    ステージセレクトのシーン遷移を設定

---
+++ Assets/Scripts/Tatsumi/StageSelectCollider.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageSelectCollider : MonoBehaviour {
+	static StageSelectCollider nowSelectCol = null;
+
+	[SerializeField, Tooltip("遷移先のシーン名")]
+	string sceneName = "";
+	[SerializeField, Tooltip("対象オブジェクトのタグ名")]
+	string targetTag = "Player";
+
+	CircleFade fade = null;
+	CircleFade Fade {
+		get {
+			if (!fade) {
+				fade = FindObjectOfType<CircleFade>();
+			}
+			return fade;
+		}
+	}
+
+	void OnCollisionEnter(Collision _col) {
+		if (_col.collider.tag != targetTag) {
+			return;
+		}
+
+		if (nowSelectCol) {
+			return;
+		}
+		nowSelectCol = this;
+
+		StartCoroutine(SelectSceneCoroutine());
+	}
+
+	IEnumerator SelectSceneCoroutine() {
+		Fade.ChangeFadeState = CircleFade.FadeState.SetOut;
+		while (true) {
+			if (Fade.ChangeFadeState == CircleFade.FadeState.Complete) {
+				break;
+			}
+			yield return null;
+		}
+
+		UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+
+		nowSelectCol = null;
+	}
+}
