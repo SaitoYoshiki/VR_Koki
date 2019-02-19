f14dce02f32717c6bf5b271311fe36c655d23260
TatsumiKei <ki3ttm@gmail.com>
2019-02-19 11:09:47 +0900

    ゲーム終了判定用のフラグを追加

---
+++ Assets/GameState.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour {
+	[SerializeField, Tooltip("ゲームクリア状態か")]
+	bool isGameClear = false;
+	public bool IsGameClear {
+		get {
+			return isGameClear;
+		}
+		set {
+			isGameClear = value;
+		}
+	}
+	[SerializeField, Tooltip("ゲームオーバー状態か")]
+	bool isGameOver = false;
+	public bool IsGameOver {
+		get {
+			return isGameOver;
+		}
+		set {
+			isGameOver = value;
+		}
+	}
+
+	[SerializeField, Tooltip("敵の数"), ReadOnly]
+	int enemyCnt = 0;
+
+	void Start() {
+		enemyCnt = FindObjectsOfType<Enemy>().Length;
+	}
+	public void PlayerBubbleBreak() {
+		IsGameOver = true;
+	}
+	public void EnemyBubbleBreak() {
+		enemyCnt--;
+
+		// 敵が全滅したら
+		IsGameClear = true;
+	}
+}
--- Assets/Scripts/Tatsumi/BubbleBreak.cs
+++ Assets/Scripts/Tatsumi/BubbleBreak.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BubbleBreak : MonoBehaviour {
+	enum BubbleType {
+		PlayerBubble,
+		EnemyBubble,
+		Other,
+	}
+	[SerializeField, Tooltip("泡の種類")]
+	BubbleType type = BubbleType.Other;
+
 	[SerializeField, Tooltip("破裂時に実行するイベント")]
 	UnityEngine.Events.UnityEvent breakEv = new UnityEngine.Events.UnityEvent();
 	public UnityEngine.Events.UnityEvent BreakEv {
@@ -16,10 +24,30 @@ public class BubbleBreak : MonoBehaviour {
 	[SerializeField, Tooltip("破裂時のパーティクルの親")]
 	Transform breakParent = null;
 
+	GameState gameSt = null;
+	GameState GameSt {
+		get {
+			if (!gameSt) {
+				gameSt = FindObjectOfType<GameState>();
+			}
+			return gameSt;
+		}
+	}
+
 	public void Break() {
+		// 敵が全滅していればプレイヤーは死なない
+		if (GameSt.IsGameClear && (type == BubbleType.PlayerBubble)) {
+			return;
+		}
+
 		BreakEv.Invoke();
 		if (breakParticle) {
 			Instantiate(breakParticle, breakParent);
+		}
+
+		// 泡の種類による処理
+		if (type != BubbleType.Other) {
+			
 		}
 	}
 	public void DestroyBreak(Transform _target) {
